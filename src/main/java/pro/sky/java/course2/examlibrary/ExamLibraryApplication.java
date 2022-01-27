b74a76e06e6b15161ebc08083d57bcb47c3bfccd
dns <pochti_zvezda@mail.ru>
2022-01-28 00:34:44 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/examlibrary/ExamLibraryApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.examlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/examlibrary/ExamLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.examlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
